2b10ceb0a67d9718c4a1d3d44ee8ab2daff1efcd
xli04 <lixu20040929@gmai.com>
2024-04-02 20:29:16 -0400

    update

--- db/Project-phase02.sql
+++ db/Project-phase02.sql
@@ -3,7 +3,7 @@ USE commerce;
 
 CREATE Table IF NOT EXISTS Small_Business_Seller
 (
-    BusinessID int          NOT NULL,
+    BusinessID int          NOT NULL AUTO_INCREMENT,
     UserName   varchar(100) NOT NULL UNIQUE,
     PassWord   int          Not NULL,
     Email      varchar(50)  NOT NULL,
@@ -13,18 +13,18 @@ CREATE Table IF NOT EXISTS Small_Business_Seller
 
 CREATE TABLE IF NOT EXISTS Orders
 (
-    OrderID         int          NOT NULL,
+    OrderID         int          NOT NULL AUTO_INCREMENT,
     Cost            int          NOT NULL,
     PlacedTime      datetime     NOT NULL,
-    Status          int          NOT NULL,
     ShippingAddress varchar(100) NOT NULL,
+    Status          int          NOT NULL,
     PRIMARY KEY (OrderID)
 );
 
 
 CREATE TABLE IF NOT EXISTS Products
 (
-    ProductID             int          NOT NULL,
+    ProductID             int          NOT NULL AUTO_INCREMENT,
     Price                 int          Not NULL,
     UnitsInStock          int          NOT NULL,
     ProductName           varchar(50)  NOT NULL,
@@ -41,7 +41,7 @@ CREATE TABLE IF NOT EXISTS Products
 
 CREATE TABLE IF NOT EXISTS Customers
 (
-    CustomerID int          NOT NULL,
+    CustomerID int          NOT NULL AUTO_INCREMENT,
     UserName   varchar(50)  NOT NULL,
     PassWord   varchar(50)  NOT NULL,
     Email      varchar(50)  NOT NULL,
@@ -60,7 +60,7 @@ CREATE TABLE IF NOT EXISTS Shippers
 
 CREATE TABLE IF NOT EXISTS ServiceRepresentative
 (
-    EmployeeID int NOT NULL,
+    EmployeeID int NOT NULL AUTO_INCREMENT,
     Phone      varchar(10),
     Name       varchar(50),
     PRIMARY KEY (EmployeeID)
@@ -70,6 +70,7 @@ CREATE TABLE IF NOT EXISTS OrderDetails
 (
     ProductID int NOT NULL,
     OrderID   int NOT NULL,
+    Quantity  int NOT NULL,
     PRIMARY KEY (ProductID, OrderID),
     CONSTRAINT fk_08 FOREIGN KEY (ProductID)
         REFERENCES Products (ProductID)
@@ -87,7 +88,7 @@ CREATE TABLE IF NOT EXISTS Card
     CardNumber     int          NOT NULL,
     ExpirationDate Date         NOT NULL,
     BillingAddress varchar(100) NOT NULL,
-    PRIMARY KEY (CustomerID),
+    PRIMARY KEY (CardNumber),
     CONSTRAINT fk_20 FOREIGN KEY (CustomerID)
         REFERENCES Customers (CustomerID)
         on update cascade on delete restrict
@@ -122,13 +123,14 @@ CREATE TABLE IF NOT EXISTS Product_In_Cart
 
 CREATE TABLE IF NOT EXISTS Service
 (
-    ServiceID  int                                         NOT NULL,
-    Type       enum ('Return','Exchange','Repair','Other') NOT NULL,
-    CustomerID int                                         NOT NULL,
-    OrderID    int                                         NOT NULL,
-    StartTime  datetime                                    NOT NULL,
-    EndTime    datetime                                    NOT NULL,
-    RepID      int                                         NOT NULL,
+    ServiceID   int                                         NOT NULL AUTO_INCREMENT,
+    Type        enum ('Return','Exchange','Repair','Other') NOT NULL,
+    CustomerID  int                                         NOT NULL,
+    OrderID     int                                         NOT NULL,
+    StartTime   datetime                                    NOT NULL,
+    EndTime     datetime                                    NOT NULL,
+    RepID       int                                         NOT NULL,
+    Description varchar(500)                                not null,
     PRIMARY KEY (ServiceID),
     CONSTRAINT fk_16 FOREIGN KEY (OrderID)
         REFERENCES Orders (OrderID)
@@ -159,7 +161,7 @@ CREATE TABLE IF NOT EXISTS Response
 
 CREATE TABLE IF NOT EXISTS Drivers
 (
-    DriverID                int          NOT NULL,
+    DriverID                int          NOT NULL AUTO_INCREMENT,
     CompanyName             varchar(100) NOT NULL,
     Age                     int          NOT NULL,
     YearsOfService          int          NOT NULL,
@@ -175,7 +177,7 @@ CREATE TABLE IF NOT EXISTS Drivers
 
 CREATE TABLE IF NOT EXISTS Shipping_Detail
 (
-    TrackingNumber          int          NOT NULL,
+    TrackingNumber          int          NOT NULL AUTO_INCREMENT,
     DriverID                int          NOT NULL,
     CompanyName             varchar(100) NOT NULL,
     Destination             varchar(100) NOT NULL,
@@ -200,16 +202,261 @@ CREATE TABLE IF NOT EXISTS Shipping_Detail
 );
 
 
+# 1.1 As a Small Business Seller, I would like to easily list my products with
+# detailed descriptions and images, so that customers can fully understand
+# and appreciate the quality and uniqueness of my offerings.
+INSERT INTO Small_Business_Seller (BusinessID, UserName, PassWord, Email, Profile)
+VALUES (1, 'Alex Rivera', 1234, 'alex.rivera@gmail.com',
+        'I am a small business seller who sells handmade jewelry.
+ I have been in business for 5 years and have a passion for creating unique pieces.');
+# 1.2 As a Small Business Seller, I want to access detailed analytics about my sales (the revenue for a specific product)
+INSERT INTO Products
+VALUES (2, 50, 10, 'NonhumanMade Necklace', 'This beautiful necklace is not handcrafted with high-quality materials
+and intricate design details.', 1, 0, 1);
+INSERT INTO Orders
+VALUES (1, 0, '2021-10-01 10:00:00', '2021-10-01 12:00:00', 1);
+INSERT INTO OrderDetails
+VALUES (2, 1, 10);
+UPDATE Orders
+SET Cost = (SELECT sum(od.Quantity * p.Price)
+            FROM OrderDetails od
+                     join Products p on od.ProductID = p.ProductID
+            WHERE od.OrderID = 1)
+WHERE OrderID = 1;
+# 1.3 As a Small Business Seller, I need to be able to manage my
+# inventory efficiently on the platform, so I can ensure that my product
+# listings are always up to date and avoid overselling or stockouts.
+INSERT INTO Products
+VALUES (1, 50, 10, 'Handmade Necklace', 'This beautiful necklace is handcrafted with high-quality materials
+and intricate design details.', 1, 0, 1);
+INSERT INTO OrderDetails
+VALUES (1, 1, 10);
+UPDATE Products
+SET UnitsSold = (SELECT sum(Quantity)
+                 FROM OrderDetails od
+                 Where od.ProductID = 1)
+WHERE ProductID = 1;
+#1.4 As a Small Business Seller, I would like to update my
+# profile to accurately reflect my up-to-date information
+UPDATE Small_Business_Seller
+SET Email   = '123@edu.neu',
+    Profile = 'The Handmade Necklace will not be available in next three weeks since the final exam'
+WHERE BusinessID = 1;
+# 1.5 As a Small Business Seller, I would like to apply for a sale for a specific product.
+UPDATE Products
+SET OnSale = true,
+    Price  = 45
+Where ProductName = 'Handmade Necklace';
+# 2.1 As a customer, I would like to filter products by local small businesses so that I can
+# support my community and find unique, locally-made items.
+INSERT INTO Customers (CustomerID, UserName, PassWord, Email, Address)
+VALUES (1, 'Aayush Bagga', 'password', 'aayush@gmail.com', '360 Huntington Ave, Boston, MA 02115');
 
--- I vote to not have this table - YuFeng
-# CREATE TABLE IF NOT EXISTS Vehicles
-# (
-#     DriverID Int         NOT NULL,
-#     Vehicle  varchar(30) NOT NULL,
-#     PRIMARY KEY (DriverID, Vehicle),
-#     CONSTRAINT fk_03
-#         FOREIGN KEY (DriverID) REFERENCES Drivers (DriverID)
-#             on update cascade on delete restrict
-# );
+
+SELECT *
+FROM Products
+WHERE BusinessID = 1;
 
 
+# 2.2 As a customer, I want to be able to add products that I wish to buy to my cart.
+INSERT INTO Cart (CustomerID, TotalItems, Total_Price)
+VALUES (1, 1, 50);
+
+#2.3 As a customer, I would like to track my order in real-time to know exactly when my items will arrive and plan accordingly and to remember when my order actually was received at my place.
+INSERT INTO Orders
+VALUES (2, 50, '2021-10-01 10:00:00', '360 Huntington Ave, Boston, MA 02115', 1);
+INSERT INTO Shippers
+VALUES ('ShippingButFast', '345 street', 3.0);
+INSERT INTO Drivers
+VALUES (1, 'ShippingButFast', 30, 5, TRUE, '123-456-7890');
+INSERT INTO Customers
+VALUES (4, '123', '456', '678@neu.edu', '123street');
+INSERT INTO Shipping_Detail (TrackingNumber, DriverID, CompanyName, Destination, Estimated_Shipping_TIme,
+                             Actual_Estimated_Time, PackageSize, OrderID, CustomerID)
+VALUES (1, 1, 'ShippingButFast', '360 Huntington Ave, Boston, MA 02115', '2021-10-01 10:00:00', '2021-10-01 12:00:00',
+        1, 1, 4);
+SELECT Shipping_Detail.Estimated_Shipping_TIme
+FROM Shipping_Detail
+WHERE CustomerID = 1;
+SELECT Shipping_Detail.Actual_Estimated_Time
+FROM Shipping_Detail
+WHERE CustomerID = 1;
+
+#2.4 As a customer, I would like to submit my service request for a specific order to address any issues or special requirements directly related to that purchase
+INSERT INTO ServiceRepresentative
+VALUES (123, '12345555', 'bb');
+INSERT INTO Service
+VALUES (1, 'Exchange', 1, 1, '2021-10-01 10:00:00', '2021-10-01 12:00:00', 123,
+        'I want to exchange the products in this order'),
+       (2, 'Repair', 1, 1, '2021-10-01 10:00:00', '2021-10-01 12:00:00', 123,
+        'I want to exchange the products in this order');
+
+#2.5 As a customer, I would like to add different payment methods
+INSERT INTO Card
+VALUES (1, 12345, '2021-10-01', '123 street'),
+       (1, 23456, '2027-10-01', '123 street');
+
+#3.1 As a Customer Service Rep, I need to have access to a
+# comprehensive customer and order database, so I can quickly
+# retrieve information and resolve any issues or inquiries that customers may have.
+INSERT INTO ServiceRepresentative
+VALUES (1, 123456, 'Ali');
+
+INSERT INTO Orders (OrderID, Cost, PlacedTime, Status, ShippingAddress)
+VALUES (3, 50, '2021-10-01 10:00:00', 1, '360 Huntington Ave, Boston, MA 02115');
+
+
+INSERT INTO Card (CustomerID, CardNumber, ExpirationDate, BillingAddress)
+VALUES (1, 0000000000, '2021-10-01 10:00:00', '360 Huntington Ave, Boston, MA 02115')
+;
+
+
+
+INSERT INTO Service
+VALUES (1111, 'Return', 1, 1, '2023-10-01 10:00:00', '2021-10-02 10:00:00', 1, 'i want t0 retur')
+;
+
+
+SELECT Customers.CustomerID, Customers.Address, Customers.Email, Customers.Username
+FROM Customers;
+
+
+SELECT Orders.OrderID, Orders.Status, Orders.ShippingAddress
+FROM Orders;
+
+
+SELECT Card.CardNumber, Card.BillingAddress, Card.ExpirationDate
+FROM Card
+         JOIN Customers ON Card.CustomerID = Customers.CustomerID;
+
+
+SELECT Service.ServiceID, Service.EndTime, Service.StartTime, Service.Type
+FROM Service
+         JOIN Customers ON Service.CustomerID = Customers.CustomerID;
+
+
+
+#3.2 As a Customer Service Rep, I want to resolve customer issues or complaints
+# by providing different services promptly, and maintain high customer satisfaction.
+INSERT INTO Orders (OrderID, Cost, PlacedTime, Status, ShippingAddress)
+VALUES (4, 50, now(), 1, '361 Huntington Ave, Boston, MA 02115');
+
+
+INSERT INTO Orders (OrderID, Cost, PlacedTime, Status, ShippingAddress)
+VALUES (5, 50, now(), 1, '362 Huntington Ave, Boston, MA 02115');
+
+
+INSERT INTO Orders (OrderID, Cost, PlacedTime, Status, ShippingAddress)
+VALUES (6, 50, now(), 1, '363 Huntington Ave, Boston, MA 02115');
+
+
+INSERT INTO Orders (OrderID, Cost, PlacedTime, Status, ShippingAddress)
+VALUES (7, 50, now(), 1, '364 Huntington Ave, Boston, MA 02115');
+
+
+
+INSERT INTO Service
+VALUES (10, 'Repair', 1, 4, NOW(), NOW(), 1, 'I want the Exchange the products');
+
+
+INSERT INTO Service
+VALUES (11, 'Return', 1, 5, NOW(), NOW(), 1, 'I want the Exchange the products');
+
+
+INSERT INTO Service
+VALUES (12, 'Exchange', 1, 6, NOW(), NOW(), 1, 'I want the Exchange the products');
+
+
+INSERT INTO Service
+VALUES (13, 'Other', 1, 7, NOW(), NOW(), 1, 'Discount code: 123');
+
+
+SELECT *
+FROM Service
+WHERE Type = 'Repair'
+ORDER BY ServiceID DESC;
+
+SELECT *
+FROM Service
+WHERE Type = 'Return'
+ORDER BY ServiceID DESC;
+
+SELECT *
+FROM Service
+WHERE Type = 'Exchange'
+ORDER BY ServiceID DESC;
+
+SELECT *
+FROM Service
+WHERE Type = 'Other'
+ORDER BY ServiceID DESC;
+
+
+
+#3.3 As a Customer Service Rep, I need to be able to manage and track the status of
+# customer issues, from the initial report to resolution, to ensure that nothing
+# falls through the cracks and customers are kept informed.
+UPDATE Service
+SET EndTime = NOW()
+WHERE OrderID = 4
+  AND Type = 'Repair';
+
+UPDATE Service
+SET EndTime = NOW()
+WHERE OrderID = 5
+  AND Type = 'Return';
+
+UPDATE Service
+SET EndTime = NOW()
+WHERE OrderID = 6
+  AND Type = 'Exchange';
+
+UPDATE Service
+SET EndTime = NOW()
+WHERE OrderID = 7
+  AND Type = 'Other';
+
+#3.4 As a Customer Service Rep, I would need to have access to a customer’s
+# order so I can help the customer to manage
+# any issues related to the order itself (applying discount code/change shipping method/recall a order)
+
+SELECT *
+FROM Orders
+WHERE OrderID = 7;
+
+
+UPDATE Orders
+SET Cost = 40
+Where OrderID = 7;
+
+#3.5As a Customer Service Rep, I need access to resources and product
+# information on the platform, so I can provide accurate and helpful
+# assistance to customers, enhancing their overall experience.
+SELECT *
+FROM OrderDetails od
+         join Orders o on od.OrderID = o.OrderID
+         join Products p on od.ProductID = p.ProductID
+WHERE o.OrderID = 7;
+
+# 4.1 As a driver, I would like to know the years of service I've worked so that I can understand my own experience level and expertise.
+INSERT INTO Drivers (DriverID, CompanyName, Age, YearsOfService, DriverLicenseExpiration, Phone)
+VALUES (2, 'ShippingButFast', 30, 5, TRUE, '123-456-7890');
+SELECT Drivers.YearsOfService
+FROM Drivers
+WHERE DRIVERID = 1;
+
+
+# 4.2 As a driver, I want to know new orders that need shipping, including details like package size, and destination, to plan my logistics effectively. (Modified since Phase 1)
+SELECT *
+FROM Shipping_Detail
+WHERE DriverID = 1;
+
+# 4.3 As a driver, I need to be able to easily update the shipping status in order to keep the seller and customer informed about the delivery progress. (Modified since Phase 1)
+UPDATE Orders
+SET Status = 2
+WHERE OrderID = 1;
+
+# 4.4 As a driver, I would like to know the estimated time of arrival for each order so that I can plan my delivery route efficiently.
+SELECT Shipping_Detail.Estimated_Shipping_TIme
+FROM Shipping_Detail
+WHERE DriverID = 1;
